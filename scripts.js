330474c5db1ee0eec7b13a6970af2002722d98bb
gytsanty <126431188+gytsanty@users.noreply.github.com>
2025-01-11 01:08:09 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,2 @@
+//Alerta
+alert("Hola soy Santiago, Técnico Universitario en Higiene y Seguridad en el Trabajo. Te presento mi curriculum vitae. Gira el dispositivo para una correcta visualización.");
